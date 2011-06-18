f5c223b3b8f67226092936f120a90a16da0e2ca8
Rob Allen <rob@akrabat.com>
2011-06-18 21:58:41 +0100

    update api-v2/public to match PEAR coding standards

--- src/api-v2/public/index.php
+++ src/api-v2/public/index.php
@@ -1,48 +1,83 @@
 <?php
-include '../inc/Request.php';
 
-// autoloader
-function __autoload($classname) {
-	if(false !== strpos($classname, '.')) {
-		// this was a filename, don't bother
-		exit;
-	}
+/**
+ * Front controller for API v2
+ *
+ * PHP version 5
+ *
+ * @category Public
+ * @package  API
+ * @author   Lorna Mitchel <lorna.mitchell@gmail.com>
+ * @author   Rob Allen <rob@akrabat.com>
+ * @license  BSD see doc/LICENSE
+ * @link     http://github.com/joindin/joind.in
+ */
 
-	if(preg_match('/[a-zA-Z]+Controller$/',$classname)) {
-		include('../controllers/' . $classname . '.php');
-		return true;
-	} elseif(preg_match('/[a-zA-Z]+Model$/',$classname)) {
-		include('../models/' . $classname . '.php');
-		return true;
-	} elseif(preg_match('/[a-zA-Z]+View$/',$classname)) {
-		include('../views/' . $classname . '.php');
-		return true;
-	}
+require '../inc/Request.php';
+
+/**
+ * Autoloader
+ * 
+ * @param string $classname name of class to load
+ * 
+ * @return boolean
+ */
+function __autoload($classname) 
+{
+    if (false !== strpos($classname, '.')) {
+        // this was a filename, don't bother
+        exit;
+    }
+
+    if (preg_match('/[a-zA-Z]+Controller$/', $classname)) {
+        include '../controllers/' . $classname . '.php';
+        return true;
+    } elseif (preg_match('/[a-zA-Z]+Model$/', $classname)) {
+        include '../models/' . $classname . '.php';
+        return true;
+    } elseif (preg_match('/[a-zA-Z]+View$/', $classname)) {
+        include '../views/' . $classname . '.php';
+        return true;
+    }
 }
 
-// Add exception handler
-function handle_exception($e) {
+
+/**
+ * Exception handler
+ * 
+ * @param Exception $e exception
+ * 
+ * @global Request $request request object
+ * 
+ * @return void
+ */
+function handleException($e)
+{
     // pull the correct format before we bail
     global $request;
     header("Status: " . $e->getCode(), false, $e->getCode());
-	$request->view->render(array($e->getMessage()));
+    $request->view->render(array($e->getMessage()));
 }
-set_exception_handler('handle_exception');
+set_exception_handler('handleException');
 
 // config setup
 define('BASEPATH', '.');
-include('../database.php');
-$ji_db = new PDO('mysql:host=' . $db['default']['hostname'] . 
+require '../database.php';
+$ji_db = new PDO(
+    'mysql:host=' . $db['default']['hostname'] . 
     ';dbname=' . $db['default']['database'],
     $db['default']['username'],
-    $db['default']['password']);
+    $db['default']['password']
+);
 
 // collect URL and headers
 $request = new Request();
 
 // set some default parameters
-$request->parameters['resultsperpage'] = $request->getParameter('resultsperpage', 20);
-$request->parameters['start'] = $request->getParameter('start', 0);
+$resultsPerPage = $request->getParameter('resultsperpage', 20);
+$start = $request->getParameter('start', 0);
+$request->parameters['resultsperpage'] = $resultsPerPage;
+$request->parameters['start'] = $start;
 
 
 // Which content type to return? Parameter takes precedence over accept headers 
@@ -52,35 +87,35 @@ $header_format = $request->preferredContentTypeOutOf($format_choices);
 $format = $request->getParameter('format', $header_format);
 
 switch ($format) {
-        case 'text/html':
-        case 'html':
-            $request->view = new HtmlView();
-            break;
-        
-        case 'application/json':
-        case 'json':
-        default:
-            $request->view = new JsonView();
-            break;
+case 'text/html':
+case 'html':
+    $request->view = new HtmlView();
+    break;
+
+case 'application/json':
+case 'json':
+default:
+    $request->view = new JsonView();
+    break;
 }
 
 $version = $request->getUrlElement(1);
 switch ($version) {
-    case 'v2':
-        // default routing for version 2
-        $return_data = routeV2($request, $ji_db);
-        break;
-    
-    case '':
-        // version parameter not specified routes to default controller
-        $defaultController = new DefaultController();
-        $return_data = $defaultController->handle($request, $ji_db);
-        break;
-    
-    default:
-        // unexpected version
-        throw new Exception('API version must be specified', 404);
-        break;
+case 'v2':
+    // default routing for version 2
+    $return_data = routeV2($request, $ji_db);
+    break;
+
+case '':
+    // version parameter not specified routes to default controller
+    $defaultController = new DefaultController();
+    $return_data = $defaultController->handle($request, $ji_db);
+    break;
+
+default:
+    // unexpected version
+    throw new Exception('API version must be specified', 404);
+    break;
 }
 
 // Handle output
@@ -89,15 +124,19 @@ $request->view->render($return_data);
 exit;
 
 /**
- *
- * @param Request $request
- * @param PDO $ji_db
+ * Router for version 2
+ * 
+ * @param Request $request request object
+ * @param PDO     $ji_db   database adapter
+ * 
  * @return array
  */
 function routeV2($request, $ji_db)
 {
     $return_data = false;
-    if(isset($request->parameters['oauth_version']) && ($request->url_elements[2] != 'oauth')) {
+    if (isset($request->parameters['oauth_version']) 
+        && ($request->url_elements[2] != 'oauth')
+    ) {
         $oauth_model = new OAuthModel();
         $oauth_model->in_flight = true;
         $oauth_model->setUpOAuthAndDb($ji_db);
@@ -105,13 +144,14 @@ function routeV2($request, $ji_db)
     }
 
     // Route: call the handle() method of the class with the first URL element
-    if(isset($request->url_elements[2])) {
+    if (isset($request->url_elements[2])) {
         $class = ucfirst($request->url_elements[2]) . 'Controller';
-        if(class_exists($class)) {
+        if (class_exists($class)) {
             $handler = new $class();
-            $return_data = $handler->handle($request, $ji_db); // the DB is set by the database config
+            $return_data = $handler->handle($request, $ji_db);
         } else {
-            throw new Exception('Unknown controller ' . $request->url_elements[2], 400);
+            $controller = $request->url_elements[2];
+            throw new Exception("Unknown controller $controller", 400);
         }
     } else {
         throw new Exception('Request not understood', 404);
